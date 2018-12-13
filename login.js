3abe4a0805adf2677acc0bf3411ae084aedf045e
bemelin <45841648+bemelin@users.noreply.github.com>
2018-12-13 12:25:40 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function validate form(){
+x=document.get Element BYJD("email").value;
+if(x==""){
+alert("email must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element BYJD("pass word").value;
+if(x==""){
+alert("email must be filled");
+return false;
+}
