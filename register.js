980e0392c314477b8b97f4746d2e4b8474019193
bemelin <45841648+bemelin@users.noreply.github.com>
2018-12-13 12:26:36 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,38 @@
+function validate form(){
+x=document.get Element BYJD("email").value;
+if(x==""){
+alert("email must be filled");
+return false;
+}
+{
+x=document.get Element BYJD("password").value;
+if(x==""){
+alert("password must be filled");
+return false;
+}
+{
+x=document.get Element BYJD("repeat password").value;
+if(x==""){
+alert("repeat password must be filled");
+return false;
+}
+x=document.get Element BYJD("user name").value;
+if(x==""){
+alert("user name must be filled");
+return false;
+}
+x=document.get Element BYJD("college name").value;
+if(x==""){
+alert("college name must be filled");
+return false;
+}
+x=document.get Element BYJD("degree").value;
+if(x==""){
+alert("degree must be filled");
+return false;
+}
+x=document.get Element BYJD("branch").value;
+if(x==""){
+alert("branch must be filled");
+return false;
+}
